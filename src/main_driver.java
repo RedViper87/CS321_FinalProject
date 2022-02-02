7e41a230df8d789cc7569242a38d4682ee085da0
RedViper87 <cp0083@uah.edu>
2022-02-01 18:31:46 -0600

    added main_driver class
    added the class: main_driver
    this will be used to run the main part of our code for the Final Project

---
+++ src/main_driver.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class main_driver {
+    /**
+     *  This is the main driver for CS321 Final Project ~ My Movie Library
+     */
+    public static void main(String[] args) {
+        //main code goes here
+        System.out.println("Hello World!"); //Because...ya gotta do this for reasons
+    }
+}
