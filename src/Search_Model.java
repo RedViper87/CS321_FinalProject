aec00fd176f1a5c06c9a813dd155c8ee80a6c6a4
RedViper87 <cp0083@uah.edu>
2022-03-26 14:19:00 -0500

    Updated Model classes
    *Review_Model attributes/functions added
    *Search_Model attributes/functions added
    *Watchlist_Model attributes/functions added

--- src/Search_Model.java
+++ src/Search_Model.java
@@ -1,2 +1,25 @@
 public class Search_Model {
+    /* ATTRIBUTES */
+    private String SearchParameter;
+    private String UserInputSearch;
+    private Movie_Library movieLibrary;
+
+    /* FUNCTIONS */
+    public void setSearchParameter(String searchParameter) {
+        SearchParameter = searchParameter;
+    }
+    public String getSearchParameter() {
+        return SearchParameter;
+    }
+
+    public void setUserInputSearch(String userInputSearch) {
+        UserInputSearch = userInputSearch;
+    }
+    public String getUserInputSearch() {
+        return UserInputSearch;
+    }
+
+    public void SearchDatabase() {
+        //some useful code goes here...probably
+    }
 }
