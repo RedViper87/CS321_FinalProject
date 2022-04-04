a24b2edb95a5c5d45d559f83fb91782391922626
lilypjaiswal <lilyj586@gmail.com>
2022-04-04 16:00:12 -0500

    Added scroll bar to main frame

--- src/Movie_Library.java
+++ src/Movie_Library.java
@@ -26,24 +26,30 @@ public class Movie_Library {
         JPanel topPanel = new JPanel();
         JPanel midPanel = new JPanel();
         JPanel botPanel = new JPanel();
-
+        JPanel all = new JPanel();
         movieLibrary.setTitle("CPE 321 Final Project - My Movie Library");
-        movieLibrary.setSize(1152,648); //keep aspect ratio same as 1920x1080
+        movieLibrary.setLayout(new BorderLayout());
+       // movieLibrary.setSize(1152,648); //keep aspect ratio same as 1920x1080
         //movieLibrary.setResizable(false);   //prevents frame from being resized
         movieLibrary.getContentPane().setBackground(new Color(70, 70, 70));    //change color of background
-        movieLibrary.setLayout(new FlowLayout());
+       // movieLibrary.setLayout(new FlowLayout());
 
         topPanel.add(userView);
-        movieLibrary.add(topPanel);
-
         midPanel.add(movieView);
-        movieLibrary.add(midPanel);
 
         botPanel.add(searchView);
         botPanel.add(watchlistView);
         botPanel.add(recommendationsView);
         botPanel.add(reviewView);
-        movieLibrary.add(botPanel);
+        all.add(topPanel);
+        all.add(midPanel);
+        all.add(botPanel);
+        BoxLayout layout = new BoxLayout(all, BoxLayout.Y_AXIS);
+        all.setLayout(layout);
+        JScrollPane scroll = new JScrollPane(all, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scroll.setPreferredSize(new Dimension(1920,1080));
+        movieLibrary.add(scroll, BorderLayout.CENTER);
+        movieLibrary.pack();
 
         movieLibrary.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
